6867f8505031832041174b72ae324b0f7d1560a4
unknown <miteshmehta2018@gmail.com>
2021-03-06 00:03:38 +0530

    Stable Committ

--- config/google.config.ts
+++ config/google.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('google', () => ({
-    googleapikey: process.env.googleapikey || 'AIzaSyARh999xf1IB1eVDaLjMnowtb7FttUimAY', //AIZa....
+    googleapikey: process.env.googleapikey || 'AIzaSyARh999xf1IB1eVDaL.......', //AIZa....
     authdomain: process.env.authdomain || 'https://fastauth-miteshmetha.firebaseapp.com'
   }));
